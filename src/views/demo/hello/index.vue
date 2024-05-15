c42712b685532bfd3a39d6286d4f119b5ac5ece2
liyatao <liyatao@yogorobot.com>
2024-05-15 20:46:25 +0800

    feat: demo search

--- src/views/demo/hello/index.vue
+++ src/views/demo/hello/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <a-space>
-    <a-button type="primary">HelloWorld</a-button>
-  </a-space>
+  <div class="demo-hello">
+    <a-card style="height: 100%">
+      <a-form layout="inline">
+        <a-form-item label="名称">
+          <a-input>输入</a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary">确定</a-button>
+        </a-form-item>
+      </a-form>
+    </a-card>
+  </div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+  .demo-hello {
+    height: 100%;
+    padding: 20px 20px 0;
+  }
+</style>
